76bfab21e95461c16cb6793a08bfab0b22ea707f
Milena <miletobon@hotmail.com>
2023-09-24 21:26:53 -0500

    admin update

--- myapp/models.py
+++ myapp/models.py
@@ -5,8 +5,14 @@ from django.shortcuts import render
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.project.name
